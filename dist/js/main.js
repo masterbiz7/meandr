705e3b0dfaf59175635a0fb5312dee45a11430c6
Leonid Markov <mypochta75@gmail.com>
2019-04-27 19:15:44 +0300

    some erorrs corrected

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,18 +1,35 @@
-function validate(form){
-	let text_fields = form.getElementsByTagName('input');
-	let textarea = document.querySelector('.textarea');
-	let validated = true;
-
-	for (let i = 0; i < text_fields.length; i++) {
-    if (text_fields[i].type != 'submit') {
-        validated = validated && (text_fields[i].value !== '');
-
-    }
-    if (!validated) {
-     alert('Fill the fields');
-		}
-		return validated;
-
-	}
+function ValidMail() {
+    var re = /^[\w-\.]+@[\w-]+\.[a-z]{2,6}$/i;
+    var myMail = document.getElementById('email').value;
+    var valid = re.test(myMail);
+    if (valid) return true;
+	    else output = 'Адрес электронной почты введен неправильно!';
+	    document.getElementById('message').innerHTML = output;
+	    return valid;
 }
 
+function ValidPhone() {
+    var re = /^\++\d[\d\(\)\ -]{4,15}\d$/;
+    var myPhone = document.getElementById('phone').value;
+    var valid = re.test(myPhone);
+	    if (valid) {output = '';}
+		    else {output = 'Номер телефона введен неправильно!';}
+		    document.getElementById('message').innerHTML = document.getElementById('message').innerHTML+'<br />'+output;
+	    return valid;
+}
+
+function ValidName()  {
+	let re = /^[A-Za-z А-Яа-я]+$/;
+	let myName = document.querySelector('.fullName').value;
+	let valid = re.test(myName);
+	var v = document.querySelector('.nameError');
+		if(valid) {
+			v.style.display="none";
+			return true;
+			} else {				
+				v.style.display="block";			
+				v.focus();
+				setTimeout(()=>{}, 3000);
+			return false;				
+				}
+}
